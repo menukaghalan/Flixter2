c7f111697524afe5405475c249be28f066470343
menukaghalan <59702166+menukaghalan@users.noreply.github.com>
2021-09-29 23:55:18 -0400

    App 2
    Second Assignment

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Menuka Ghalan on 9/29/21.
+//  Copyright © 2021 Menuka Ghalan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+
+
+
+
+}
